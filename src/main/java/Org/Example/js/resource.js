0803f0e82eb24a29c1e3af284ede582ffd38a03a
Magnus Munk Braunstein <147395199+MagnusMunkBraunstein@users.noreply.github.com>
2024-12-04 12:14:47 +0100

    Save ressource - not testet function work on ressource page

--- src/main/java/Org/Example/js/resource.js
+++ src/main/java/Org/Example/js/resource.js
@@ -6,4 +6,32 @@ document.getElementById("saveButton").addEventListener("click", () => {
     setTimeout(() => {
         confirmationMessage.classList.add("hidden");
     }, 3000);
+    saveResource();
+
 });
+
+function saveResource(){
+    const url = "http://localhost:8080/api/resource/save";
+    const data = {
+        name: "Resource name",
+        author: "Resource author",
+        fromDate: "2023-01-01",
+        toDate: "2023-02-01",
+        users: []
+    };
+
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Success:", data);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+}
