ae84a729e503c1fdedad859cd9895a587d33cb2c
Magnus Munk Braunstein <147395199+MagnusMunkBraunstein@users.noreply.github.com>
2024-12-05 13:09:05 +0100

    Save Resouce

--- src/main/java/Org/Example/js/resource.js
+++ src/main/java/Org/Example/js/resource.js
@@ -77,3 +77,26 @@ function loadPersonalResources() {
             console.error("Error:", error);
         });
 }
+
+
+function saveResource() {
+    const url = "http://localhost:8080/api/users/saveaspersonalresource";
+    const params = new URLSearchParams({
+        resourceId: "3",
+        userId: "3"
+    });
+
+    fetch(url + "?" + params.toString(), {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Success:", data);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+}
